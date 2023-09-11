b161771056edceb36271b108f8f8b0dbad220bf1
MARIEM0123 <m00riemhissou@gmail.com>
2023-09-11 16:21:07 +0000

    liste & tuples

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    """The function to return the length
-	of a string and its first character."""
+    """Return the length and first character of a string."""
     if sentence == "":
         return (0, None)
     return (len(sentence), sentence[0])
