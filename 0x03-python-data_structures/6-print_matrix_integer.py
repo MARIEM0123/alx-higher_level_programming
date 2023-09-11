105e3cc34e89de1379ebe0df6c919a83cd0c00fc
MARIEM0123 <m00riemhissou@gmail.com>
2023-09-11 20:14:56 +0000

    Tulipes & lists

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-"""Write all characters in a given matrix."""
-    for row in matrix:
-        for col in row:
-            print("{:d}".format(col), end=" " if col != row[-1] else "")
-        print()
+	"""Write all characters in a given matrix."""
+	for row in matrix:
+		for col in row:
+			print("{:d}".format(col), end=" " if col != row[-1] else "")
+			print()
