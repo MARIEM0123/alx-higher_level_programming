b2613a3b860dbce61590ccf49291cd7cd92fe33d
MARIEM0123 <m00riemhissou@gmail.com>
2023-10-02 13:19:26 -0700

    more classes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Rectangle class definition"""
+
+
+class Rectangle:
+    """the class for rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialisation for new Rectangle.
+
+        Args:
+            width: The width of the new rectangle
+            height: The height of the new rectangle
+        """
+        self.width = width
+        self.height = height
+
+    def width(self):
+        """the function for the width of the rectangle."""
+        return self.__width
+
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def height(self):
+        """The function to get the height of the rectangle."""
+        return self.__height
+
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
